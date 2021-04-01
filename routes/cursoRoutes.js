2be2479c0ae637447a2541bf76e9d425a7e6047e
Sebastián Blanc <blancseba@gmail.com>
2021-04-01 11:52:48 -0300

    cambiada rutas de cursos

--- routes/cursoRoutes.js
+++ routes/cursoRoutes.js
@@ -3,7 +3,7 @@ const cursoController = require('../controllers/curso');
 module.exports = (app) => {
     app.post('/api/curso/create', cursoController.create);
     app.put('/api/curso/update', cursoController.update);
-    app.delete('/api/curso/delete', cursoController.delete);
+    app.post('/api/curso/delete', cursoController.delete);
     app.get('/api/curso/all', cursoController.findAll);
-    app.get('/api/curso/find', cursoController.find);
+    app.post('/api/curso/find', cursoController.find);
 }
